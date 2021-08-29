ed2a25f50493e3066db560313c4ad3fc383d938a
KseniyaSokolovskaya <deff.1990@gmail.com>
2021-08-29 19:06:14 +0300

    feat: add solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    bracketsConfig = bracketsConfig.map(item => item.join(''));
+    let check;
+
+    if (str.length % 2 !== 0) {
+        return 0;
+    } else {
+        while (true) {
+            check = false;
+            bracketsConfig.forEach((item) => {
+                if (str.includes(item)) {
+                    str = str.replace(item, '');
+                    check = true;
+                }
+            })
+            if (!check) break;
+        }
+
+    }
+    return str.length === 0 ? true : false;
 }
